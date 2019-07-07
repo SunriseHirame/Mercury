c86e975d3e97ccf4de4630afc3458de8369ebf1b
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-07-07 20:48:14 +0300

    Split Hierarchy Utility to Editor and Runtime parts

---
+++ Editor/HierarchyUtilityMenuItems.cs
@@ -0,0 +1,66 @@
+﻿using UnityEditor;
+using UnityEngine;
+using PivotPosition = Hirame.Mercury.HierarchyUtility.PivotPosition;
+
+namespace Hirame.Mercury.Editor
+{
+    public class HierarchyUtilityMenuItems : MonoBehaviour
+    {
+        private const int PivotMenuPriority = -10;
+
+
+        [MenuItem ("GameObject/Pivot/Center", false, PivotMenuPriority)]
+        private static void SetPivotToCenter (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Center);
+        }
+
+        [MenuItem ("GameObject/Pivot/Bottom", false, PivotMenuPriority)]
+        private static void SetPivotToBottom (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Bottom);
+        }
+
+        [MenuItem ("GameObject/Pivot/Top", false, PivotMenuPriority)]
+        private static void SetPivotToTop (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Top);
+        }
+
+        [MenuItem ("GameObject/Pivot/Left", false, PivotMenuPriority)]
+        private static void SetPivotToLeft (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Left);
+        }
+
+        [MenuItem ("GameObject/Pivot/Right", false, PivotMenuPriority)]
+        private static void SetPivotToRight (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Right);
+        }
+
+        [MenuItem ("GameObject/Pivot/Back", false, PivotMenuPriority)]
+        private static void SetPivotToBack (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Back);
+        }
+
+        [MenuItem ("GameObject/Pivot/Front", false, PivotMenuPriority)]
+        private static void SetPivotToFront (MenuCommand command)
+        {
+            SetPivot (command, PivotPosition.Front);
+        }
+
+        private static void SetPivot (MenuCommand command, PivotPosition pivot)
+        {
+            var gameObject = command.context as GameObject;
+            if (gameObject == null)
+                return;
+
+            var childMeshFilters = gameObject.GetComponentsInChildren<MeshFilter> ();
+            var offset = HierarchyUtility.GetPivotOffset (childMeshFilters, pivot);
+
+            HierarchyUtility.OffsetChildren (gameObject.transform, offset);
+        }
+    }
+}
---
+++ Runtime/HierarchyUtility.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+namespace Hirame.Mercury
+{
+    public static class HierarchyUtility
+    {
+        public enum PivotPosition
+        {
+            Center,
+            Top,
+            Bottom,
+            Left,
+            Right,
+            Front,
+            Back
+        }
+        
+
+        public static Vector3 GetPivotOffset (MeshFilter[] meshFilters, PivotPosition pivotPosition)
+        {
+            var center = new Vector3 ();
+            var offset = new Vector3 ();
+
+            foreach (var meshFilter in meshFilters)
+            {
+                var filterTransform = meshFilter.transform;
+
+                var sMesh = meshFilter.sharedMesh;
+                var bounds = sMesh.bounds;
+
+                center += filterTransform.InverseTransformPoint (bounds.center);
+
+                if (pivotPosition != PivotPosition.Center)
+                    UpdateOffset (ref offset, in bounds, pivotPosition);
+            }
+
+            if (pivotPosition == PivotPosition.Center && meshFilters.Length > 0)
+                center /= meshFilters.Length;
+
+            return center + offset;
+        }
+        
+        public static void OffsetChildren (Transform transform, in Vector3 offset)
+        {
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild (i).localPosition += offset;
+            }
+        }
+
+        private static void UpdateOffset (ref Vector3 offset, in Bounds bounds, PivotPosition pivot)
+        {
+            var extents = bounds.extents;
+            switch (pivot)
+            {
+                case PivotPosition.Center:
+                    break;
+                case PivotPosition.Left:
+                    if (offset.x < extents.x)
+                        offset.x = extents.x;
+                    break;
+                case PivotPosition.Right:
+                    if (offset.x > -extents.x)
+                        offset.x = -extents.x;
+                    break;
+                case PivotPosition.Top:
+                    if (offset.y > -extents.y)
+                        offset.y = -extents.y;
+                    break;
+                case PivotPosition.Bottom:
+                    if (offset.y < extents.y)
+                        offset.y = extents.y;
+                    break;
+                case PivotPosition.Front:
+                    if (offset.z > -extents.z)
+                        offset.z = -extents.z;
+                    break;
+                case PivotPosition.Back:
+                    if (offset.z < extents.z)
+                        offset.z = extents.z;
+                    break;
+            }
+        }
+    }
+}
