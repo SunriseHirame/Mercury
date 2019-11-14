3f152c0d20b0345c3737af0abf9e9d14180aa1ab
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-11-14 22:19:14 +0200

    Added ability to find script files from context menu

---
+++ Editor/Utilities/PingScriptAsset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+
+namespace Hirame.Mercury.Editor
+{
+
+
+    public static class PingScriptAsset
+    {
+        [MenuItem ("CONTEXT/MonoBehaviour/Highlight Script Asset")]
+        public static void MonoBehaviourSelectScriptAsset (MenuCommand command)
+        {
+            var fileName = $"{command.context.GetType ().Name}.cs";
+
+            var scripts = AssetDatabase.FindAssets ("t:script");
+
+            foreach (var t in scripts)
+            {
+                var path = AssetDatabase.GUIDToAssetPath (t);
+                var startIndex = path.LastIndexOf ('/') + 1;
+                var name = path.Substring (startIndex);
+
+                if (!name.Equals (fileName))
+                    continue;
+
+                var obj = AssetDatabase.LoadAssetAtPath<MonoScript> (path);
+                EditorGUIUtility.PingObject (obj);
+                break;
+            }
+        }
+    }
+}
